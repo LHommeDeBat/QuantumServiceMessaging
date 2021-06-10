4a5de0f84ca1fc02237667fad46e235a8c8c7f58
Stefan Basaric <stefan.basaric@hotmail.de>
2021-06-10 18:00:17 +0200

    Initial commit

---
+++ src/main/java/de/unistuttgart/iaas/messaging/quantumservice/QuantumServiceApplication.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.iaas.messaging.quantumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumServiceApplication.class, args);
+    }
+}
---
+++ src/test/java/de/unistuttgart/iaas/messaging/quantumservice/QuantumServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package de.unistuttgart.iaas.messaging.quantumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
