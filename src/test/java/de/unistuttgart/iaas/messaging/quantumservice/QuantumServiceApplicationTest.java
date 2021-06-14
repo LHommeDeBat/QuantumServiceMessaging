79ddd7b3d4a1f0adb877006fd7c3f4a5b23e2995
Stefan Basaric <stefan.basaric@hotmail.de>
2021-06-14 18:55:18 +0200

    Readded test for executing python script

---
+++ src/test/java/de/unistuttgart/iaas/messaging/quantumservice/QuantumServiceApplicationTest.java
@@ -0,0 +1,24 @@
+package de.unistuttgart.iaas.messaging.quantumservice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumServiceApplicationTests {
+
+    @Test
+    void testExec() throws IOException {
+        String[] command = new String[]{"python", "actions/TestQuantumScript.py", "agrar"};
+        Process p = Runtime.getRuntime().exec(command);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            System.out.println(line + "\n");
+        }
+    }
+}
