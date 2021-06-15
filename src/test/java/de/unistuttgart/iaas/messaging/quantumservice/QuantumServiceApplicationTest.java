7a9202da57650a90c33dfe2bc32c9d873504a2bd
Stefan Basaric <stefan.basaric@hotmail.de>
2021-06-15 17:15:41 +0200

    Adjusted test

--- src/test/java/de/unistuttgart/iaas/messaging/quantumservice/QuantumServiceApplicationTest.java
+++ src/test/java/de/unistuttgart/iaas/messaging/quantumservice/QuantumServiceApplicationTest.java
@@ -1,6 +1,7 @@
 package de.unistuttgart.iaas.messaging.quantumservice;
 
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -12,13 +13,33 @@ class QuantumServiceApplicationTests {
 
     @Test
     void testExec() throws IOException {
-        String[] command = new String[]{"python", "actions/TestQuantumScript.py", "agrar"};
-        Process p = Runtime.getRuntime().exec(command);
+        Process p = Runtime.getRuntime().exec(new String[] {"python", "actions/main.py"});
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
         String line;
         while ((line = reader.readLine()) != null) {
             System.out.println(line + "\n");
         }
+    }
+
+    @Test
+    void testExecViaFile() throws IOException {
+        FileWriter fw = new FileWriter("actions/main.py");
+
+        fw.write("import json");
+        fw.write(System.lineSeparator());
+        fw.write("import numpytest");
+        fw.write(System.lineSeparator());
+        fw.write(System.lineSeparator());
+        fw.write("x = '{\"name\": \"Stefan\"}'");
+        fw.write(System.lineSeparator());
+        fw.write("y = json.loads(x)");
+        fw.write(System.lineSeparator());
+        fw.write(System.lineSeparator());
+        fw.write("result = numpytest.main(y)");
+        fw.write(System.lineSeparator());
+        fw.write("print(result)");
+
+        fw.close();
     }
 }
